2b99048ce121ce3da174c6b1797e54bb43599e04
Abhishek Malali <abhishekmalali@gmail.com>
2016-04-20 02:07:55 -0400

    Adding base and result generation files with the test notebook

---
+++ basic_lstm.py
@@ -0,0 +1,40 @@
+import theano
+import theano.tensor as T
+from layers import LSTMLayer, InputLayer, FullyConnectedLayer
+from lib import get_params, make_caches, SGD, momentum
+
+class basicLSTM():
+
+    def __init__(self, num_input, num_cells=50, num_output=1):
+        X = T.matrix('x')
+        Y = T.matrix('y')
+        eta = T.scalar('eta')
+        alpha = T.scalar('alpha')
+
+        self.num_input = num_input
+        self.num_output = num_output
+        self.num_cells = num_cells
+        self.eta = eta
+
+        inputs = InputLayer(X, name="inputs")
+        lstm = LSTMLayer(num_input, num_cells, input_layer=inputs, name="lstm")
+        fc = FullyConnectedLayer(num_cells, num_output, input_layer=lstm)
+        Y_hat = T.mean(fc.output(), axis=2)
+        layer = inputs, lstm, fc
+        self.params = get_params(layer)
+        self.caches = make_caches(self.params)
+        self.layers = layer
+        mean_cost = T.mean((Y - Y_hat)**2)
+        last_cost = T.mean((Y[-1] - Y_hat[-1])**2)
+        self.cost = alpha*mean_cost + (1-alpha)*last_cost
+        self.updates = momentum(self.cost, self.params, self.caches, self.eta, clip_at=3.0)
+        self.train = theano.function([X, Y, eta, alpha], [self.cost, last_cost] ,\
+                updates=self.updates, allow_input_downcast=True)
+        self.costfn = theano.function([X, Y, alpha], [self.cost, last_cost],\
+                allow_input_downcast=True)
+        self.predict = theano.function([X], [Y_hat], allow_input_downcast=True)
+
+
+    def reset_state(self):
+        for layer in self.layers:
+            layer.reset_state() 
---
+++ irregular_ts.py
@@ -0,0 +1,94 @@
+import numpy as np
+import os
+#Setting the seed before other dependent imports
+np.random.seed(0)
+import theano
+import theano.tensor as T
+import pandas as pd
+import matplotlib.pyplot as plt
+from layers import LSTMLayer, InputLayer, FullyConnectedLayer
+from lib import get_params, make_caches, SGD, momentum
+from basic_lstm import basicLSTM
+
+def basic_irregular(stopTime, numPoints, numSelPoints):
+    time = np.linspace(0, stopTime, numPoints)
+    data = np.sin(time)
+    index = np.sort(np.random.choice(range(numPoints), size=numSelPoints, replace=False))
+    time_irr = time[index]
+    data_irr = data[index]
+    delta_t = [0] + list(np.array(time_irr[1:]) - np.array(time_irr[:-1]))
+    data_mat = np.matrix([list(data_irr), delta_t]).T
+    data_mat = data_mat[:-1,:]
+    resp_mat = np.matrix(np.roll(data_irr, -1)[:-1]).T
+    time_vec = time_irr[1:]
+    return data_mat, resp_mat, time_vec
+
+def plot_predicted_series(lstm_model, data_mat, resp_mat, time_vec, it_num, n_prev, base_path):
+    plt.plot(time_vec, lstm_model.predict(data_mat)[0], label='Predicted' )
+    plt.plot(time_vec, resp_mat, label='Actual')
+    plt.title('Predicted plots for size '+str(n_prev)+' and '+str(it_num)+\
+            ' training iterations')
+    plt.xlabel('Time')
+    plt.ylabel('Magnitude')
+    plt.legend()
+    plt.ylim([-2, 2])
+    plt.savefig(base_path+'predicted_n_prev_'+str(n_prev)+'_iteration_'+str(it_num)+\
+            '.png')
+    plt.close()
+
+def plot_error(error_df, n_prev, numCells, base_path, Training=True):
+    if Training == True:
+        str_val = 'Training'
+    else:
+        str_val = 'Test'
+    plt.plot(error_df.index, error_df.Error)
+    plt.title(str_val+' Error variation with Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel(str_val+' Error')
+    plt.savefig(base_path+'error_n_prev_'+str(n_prev)+'_numcells_'+str(numCells)+'.png')
+    plt.close()
+
+
+graphs_path = os.path.join(os.getcwd(),'graphs')
+store_path = os.path.join(graphs_path, 'basic-irregular-time-intervals')
+pred_path = os.path.join(store_path, 'predicted-graphs')
+err_path = os.path.join(store_path, 'error-graphs')
+if not os.path.isdir(err_path):
+    os.makedirs(err_path)
+err_path = err_path + '/'
+
+stopTime = 20
+numPoints = 400
+numSelPoints = 60
+data_mat, resp_mat, time_vec = basic_irregular(stopTime, numPoints, numSelPoints)
+#eta - learning rate
+eta = 0.5
+#alpha - momentum hyperparameter
+alpha = 1.0
+n_iterations = 1000
+numCellArray = np.arange(5, 30, 5)
+n_prev = 1
+
+for num in numCellArray:
+    #Creating the folder for storing the folders
+    numPath = os.path.join(pred_path, str(num)+'cell_states')
+    if not os.path.isdir(numPath):
+        os.makedirs(numPath)
+    numPath = numPath + '/'
+    lstm_model = basicLSTM(2, num_cells=num)
+    error_list = []
+    for i in range(n_iterations):
+        r_cost = lstm_model.train(data_mat, resp_mat, eta, alpha)
+        error_list.append([i+1, float(r_cost[0])])
+        if (i+1)<100 and (i+1)%10 == 0:
+            print "iteration: %s, cost: %s" % (i+1, float(r_cost[0]))
+            plot_predicted_series(lstm_model, data_mat, resp_mat,\
+                    time_vec, i+1, n_prev, numPath)
+        if (i+1)%100 == 0:
+            print "iteration: %s, cost: %s" % (i+1, float(r_cost[0]))
+            plot_predicted_series(lstm_model, data_mat, resp_mat,\
+                    time_vec, i+1, n_prev, numPath)
+    error_df = pd.DataFrame(np.array(error_list), columns=['Iteration','Error'])
+    error_df = error_df.set_index('Iteration')
+    error_df.to_csv(err_path+'error_trajectory_numcells_'+str(num)+'.csv')
+    plot_error(error_df, n_prev, num, err_path)
